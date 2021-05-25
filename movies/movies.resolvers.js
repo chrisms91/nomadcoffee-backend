0f6ec06b90f95d9e1587415ef2d80c8802bff613
Christopher Kim <chrisms91@gmail.com>
2021-05-24 23:16:00 -0700

    #3.9 Architecture part One

---
+++ movies/movies.resolvers.js
@@ -0,0 +1,42 @@
+import client from './client';
+
+const resolvers = {
+  Query: {
+    movies: () => client.movie.findMany(),
+    movie: (_, { id }) =>
+      client.movie.findUnique({
+        where: {
+          id,
+        },
+      }),
+  },
+  Mutation: {
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
+    deleteMovie: (_, { id }) =>
+      client.movie.delete({
+        where: {
+          id,
+        },
+      }),
+    updateMovie: (_, { id, title, year, genre }) =>
+      client.movie.update({
+        where: {
+          id,
+        },
+        data: {
+          title,
+          genre,
+          year,
+        },
+      }),
+  },
+};
+
+export default resolvers;
